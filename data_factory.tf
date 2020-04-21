768fcdf215751b97192922fa0e8844328a17c48d
Sam Debruyn <sam@debruyn.dev>
2020-04-21 15:04:15 +0200

    use key instead of key ID for data factory service principal

--- data_factory.tf
+++ data_factory.tf
@@ -12,5 +12,5 @@ resource "azurerm_data_factory_linked_service_data_lake_storage_gen2" "dfdsl" {
   tenant                = data.azurerm_client_config.current.tenant_id
   url                   = "https://${azurerm_storage_account.dls.name}.blob.core.windows.net/${azurerm_storage_data_lake_gen2_filesystem.dlfs.name}"
   service_principal_id  = azuread_service_principal.oidc_principal.object_id
-  service_principal_key = azuread_application_password.aadapp-srv.id // TODO: verify if we need ID or key itself
+  service_principal_key = random_password.oidc_client_secret.result
 }
