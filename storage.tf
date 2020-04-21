2be46f8acf17e165f26f14cb52e321db6fe67f05
Samuel Dumont <samuel@dataroots.io>
2020-04-21 16:31:00 +0200

    Adding new permissions to the blob storage

--- storage.tf
+++ storage.tf
@@ -13,8 +13,20 @@ resource "azurerm_storage_data_lake_gen2_filesystem" "dlfs" {
   storage_account_id = azurerm_storage_account.dls.id
 }
 
-resource "azurerm_role_assignment" "oidcsa" {
+resource "azurerm_role_assignment" "service_account_storage_account_owner" {
   scope                = azurerm_resource_group.rg.id
   role_definition_name = "Owner"
+  principal_id         = azuread_service_principal.sp.object_id
+}
+
+resource "azurerm_role_assignment" "service_account_storage_account_blob_data_owner" {
+  scope                = azurerm_storage_account.dls.id
+  role_definition_name = "Storage Blob Data Owner"
+  principal_id         = azuread_service_principal.sp.object_id
+}
+
+resource "azurerm_role_assignment" "current_user_storage_account_blob_data_owner" {
+  scope                = data.azurerm_client_config.current.client_id
+  role_definition_name = "Storage Blob Data Owner"
   principal_id         = azuread_service_principal.sp.object_id
 }
