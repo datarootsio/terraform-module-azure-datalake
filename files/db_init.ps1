3ee60ad81c795b7aa55711041a31c1e94e5a5088
Samuel Dumont <samuel@dataroots.io>
2020-04-22 18:16:01 +0200

    Fixing the script

--- files/db_init.ps1
+++ files/db_init.ps1
@@ -4,8 +4,8 @@ $SqlConnection = new-object System.Data.SqlClient.SqlConnection
 $SqlConnection.ConnectionString = 'Server=tcp:${server}.database.windows.net,1433;Initial Catalog=${database};Persist Security Info=False;User ID=${user};Password=${password};'
 
 $SqlCommand = $SqlConnection.CreateCommand()
-$SqlCommand.CommandText = get-content "/tmp/rendered_script.sql
+$SqlCommand.CommandText = get-content "/tmp/rendered_script.sql"
 
 $SqlConnection.Open();
-$SqlCommand.ExecuteQuery();
+$SqlCommand.ExecuteNonQuery();
 $SqlConnection.Close();
