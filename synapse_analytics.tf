e998cc3f2174a8b767e3c36acf92178ff562f410
Sam <sam@debruyn.dev>
2020-04-22 09:23:45 +0200

    Add an Azure Synapse Analytics instance (#11)
    * fixed incorrect usage of OIDC naming and service principal / app corrections
    
    * fixed permissions for data factory sp to access sa
    
    * add synapse analytics (WIP)
    
    * Add a Synapse Analytics instance

---
+++ synapse_analytics.tf
@@ -0,0 +1,28 @@
+resource "random_password" "synapse_srv_pw" {
+  length = 32
+}
+
+resource "random_string" "synapse_srv_admin" {
+  length  = 8
+  special = false
+}
+
+resource "azurerm_sql_server" "synapse_srv" {
+  name                         = "dwsrv${var.data_lake_name}"
+  location                     = var.region
+  resource_group_name          = azurerm_resource_group.rg.name
+  tags                         = azurerm_resource_group.rg.tags
+  version                      = "12.0"
+  administrator_login          = random_string.synapse_srv_admin.result
+  administrator_login_password = random_password.synapse_srv_pw.result
+}
+
+resource "azurerm_sql_database" "synapse" {
+  name                             = "dw${var.data_lake_name}"
+  location                         = var.region
+  resource_group_name              = azurerm_resource_group.rg.name
+  server_name                      = azurerm_sql_server.synapse_srv.name
+  tags                             = azurerm_resource_group.rg.tags
+  edition                          = "DataWarehouse"
+  requested_service_objective_name = var.data_warehouse_dtu
+}
