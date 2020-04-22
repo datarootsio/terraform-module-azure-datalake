6df4512b6c255623a349713a9937dd6307339ab0
Sam Debruyn <sam@debruyn.dev>
2020-04-22 16:09:10 +0200

    use passwords from input vars for synapse

--- synapse_analytics.tf
+++ synapse_analytics.tf
@@ -1,20 +1,11 @@
-resource "random_password" "synapse_srv_pw" {
-  length = 32
-}
-
-resource "random_string" "synapse_srv_admin" {
-  length  = 8
-  special = false
-}
-
 resource "azurerm_sql_server" "synapse_srv" {
   name                         = "dwsrv${var.data_lake_name}"
   location                     = var.region
   resource_group_name          = azurerm_resource_group.rg.name
   tags                         = azurerm_resource_group.rg.tags
   version                      = "12.0"
-  administrator_login          = random_string.synapse_srv_admin.result
-  administrator_login_password = random_password.synapse_srv_pw.result
+  administrator_login          = var.sql_server_admin_username
+  administrator_login_password = var.sql_server_admin_password
 }
 
 resource "azurerm_sql_database" "synapse" {
