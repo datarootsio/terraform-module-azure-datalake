1a354c97718983bc9f78d3812bb54d87bae8c0dd
samueldumont <samuel@dumont.info>
2020-04-24 10:43:34 +0200

    Fixing the token script (#22)
    * Adding databricks cluster + script to generate the token
    
    * Linting
    
    * Fixing the token generation script
    
    * Fixing the lint
    
    Co-authored-by: Samuel Dumont <samuel@dataroots.io>

--- databricks.tf
+++ databricks.tf
@@ -6,3 +6,21 @@ resource "azurerm_databricks_workspace" "dbks" {
   sku                         = var.databricks_sku
   tags                        = azurerm_resource_group.rg.tags
 }
+
+data "external" "databricks_token" {
+  depends_on = [azurerm_databricks_workspace.dbks]
+  program    = ["bash", "${path.module}/files/generate_databricks_token.sh"]
+  query = {
+    # arbitrary map from strings to strings, passed
+    # to the external program as the data query.
+    region               = lower(replace(var.region, " ", ""))
+    databricks_workspace = azurerm_databricks_workspace.dbks.name
+    resource_group       = azurerm_resource_group.rg.name
+  }
+}
+
+module "databricks_sample_data" {
+  source = "./databricks_sample_data"
+  host   = "https://${lower(replace(var.region, " ", ""))}.azuredatabricks.net"
+  token  = data.external.databricks_token.result["token"]
+}
