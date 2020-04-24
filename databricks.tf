1783d797a525a5e75a9913a59a34e55f1618ea05
Sam <sam@debruyn.dev>
2020-04-24 16:08:57 +0200

    Changed the databricks token script (#26)
    * modified the databricks token script
    
    * Formatting
    
    Co-authored-by: samueldumont <samuel@dumont.info>

--- databricks.tf
+++ databricks.tf
@@ -7,20 +7,27 @@ resource "azurerm_databricks_workspace" "dbks" {
   tags                        = azurerm_resource_group.rg.tags
 }
 
-data "external" "databricks_token" {
-  depends_on = [azurerm_databricks_workspace.dbks]
-  program    = ["bash", "${path.module}/files/generate_databricks_token.sh"]
-  query = {
-    # arbitrary map from strings to strings, passed
-    # to the external program as the data query.
-    region               = lower(replace(var.region, " ", ""))
-    databricks_workspace = azurerm_databricks_workspace.dbks.name
-    resource_group       = azurerm_resource_group.rg.name
+resource "null_resource" "databricks_token" {
+  triggers = {
+    build_number = timestamp()
   }
+
+  provisioner "local-exec" {
+    command = "${path.module}/files/generate_databricks_token.sh > /tmp/databricks_token.txt"
+    environment = {
+      DATABRICKS_WORKSPACE_RESOURCE_ID = azurerm_databricks_workspace.dbks.id
+      DATABRICKS_ENDPOINT              = "https://${var.region}.azuredatabricks.net"
+    }
+  }
+}
+
+data "local_file" "databricks_token" {
+  depends_on = [null_resource.databricks_token]
+  filename   = "/tmp/databricks_token.txt"
 }
 
 module "databricks_sample_data" {
   source = "./databricks_sample_data"
   host   = "https://${lower(replace(var.region, " ", ""))}.azuredatabricks.net"
-  token  = data.external.databricks_token.result["token"]
+  token  = trimspace(data.local_file.databricks_token.content)
 }
--- files/generate_databricks_token.sh
+++ files/generate_databricks_token.sh
@@ -1,61 +1,47 @@
-#!/bin/bash
+#!/bin/sh
 
-# Change these values.
-# Use a Client ID with Contributor permissions
-#   on the Databricks workspace.
+set -e
 
 function parse_input() {
-  # jq reads from stdin so we don't have to set up any inputs, but let's validate the outputs
-  eval "$(jq -r '@sh "REGION=\(.region) DATABRICKS_WORKSPACE=\(.databricks_workspace) RESOURCE_GROUP=\(.resource_group)"')"
-  if [[ -z "${DATABRICKS_WORKSPACE}" ]]; then export DATABRICKS_WORKSPACE=none; fi
-  if [[ -z "${REGION}" ]]; then export REGION=none; fi
-  if [[ -z "${RESOURCE_GROUP}" ]]; then export RESOURCE_GROUP=none; fi
-}
-
-function exit_with_dummy_token() {
-    jq -n --arg pat_token "thisisadummytoken" '{"token":$pat_token}'
-    exit 0
+  test -n "$DATABRICKS_WORKSPACE_RESOURCE_ID"
+  test -n "$DATABRICKS_ENDPOINT"
 }
 
 function produce_output() {
-
-  wsId=$(az resource show \
-    --resource-type Microsoft.Databricks/workspaces \
-    -g "$RESOURCE_GROUP" \
-    -n "$DATABRICKS_WORKSPACE" \
-    --query id -o tsv) || exit_with_dummy_token
-
   # Get a token for the global Databricks application.
   # The resource name is fixed and never changes.
   token_response=$(az account get-access-token --resource 2ff814a6-3304-4ab8-85cb-cd0e6f879c1d)
-  token=$(jq .accessToken -r <<< "$token_response") || exit_with_dummy_token
+  token=$(jq .accessToken -r <<< "$token_response")
 
   # Get a token for the Azure management API
   token_response=$(az account get-access-token --resource https://management.core.windows.net/)
   azToken=$(jq .accessToken -r <<< "$token_response")
 
-  get_existing_token=$(curl -sf https://$REGION.azuredatabricks.net/api/2.0/token/list \
+  get_existing_token=$(curl -sf $DATABRICKS_ENDPOINT/api/2.0/token/list \
       -H "Authorization: Bearer $token" \
       -H "X-Databricks-Azure-SP-Management-Token:$azToken" \
-      -H "X-Databricks-Azure-Workspace-Resource-Id:$wsId")
+      -H "X-Databricks-Azure-Workspace-Resource-Id:$DATABRICKS_WORKSPACE_RESOURCE_ID")
 
-  existing_token=$(jq '.token_infos[]  | select(.comment == "Default Terraform Token") | .token_id' -r <<< "$get_existing_token") || exit_with_dummy_token
+  if [[ ! -z "$get_existing_token"  ]] && [ "${get_existing_token}" != "{}" ]; then
+    existing_token=$(jq '.token_infos[]  | select(.comment == "Default Terraform Token") | .token_id' -r <<< "$get_existing_token")
+  fi
 
   if [[ -n "$existing_token" ]]; then
-      get_existing_token=$(curl -sf https://$REGION.azuredatabricks.net/api/2.0/token/delete \
+      get_existing_token=$(curl -sf $DATABRICKS_ENDPOINT/api/2.0/token/delete \
       -H "Authorization: Bearer $token" \
       -H "X-Databricks-Azure-SP-Management-Token:$azToken" \
-      -H "X-Databricks-Azure-Workspace-Resource-Id:$wsId" \
+      -H "X-Databricks-Azure-Workspace-Resource-Id:$DATABRICKS_WORKSPACE_RESOURCE_ID" \
       -d '{"token_id":"'"$existing_token"'"}')
   fi
+
   # You can also generate a PAT token. Note the quota limit of 600 tokens.
-  api_response=$(curl -sf https://$REGION.azuredatabricks.net/api/2.0/token/create \
+  api_response=$(curl -sf $DATABRICKS_ENDPOINT/api/2.0/token/create \
     -H "Authorization: Bearer $token" \
     -H "X-Databricks-Azure-SP-Management-Token:$azToken" \
-    -H "X-Databricks-Azure-Workspace-Resource-Id:$wsId" \
-    -d '{ "lifetime_seconds": 3600, "comment": "Default Terraform Token" }') || exit_with_dummy_token
+    -H "X-Databricks-Azure-Workspace-Resource-Id:$DATABRICKS_WORKSPACE_RESOURCE_ID" \
+    -d '{ "lifetime_seconds": 3600, "comment": "Default Terraform Token" }')
   pat_token=$(jq .token_value -r <<< "$api_response")
-  jq -n --arg pat_token "$pat_token" '{"token":$pat_token}'
+  echo $pat_token
 }
 
 parse_input
