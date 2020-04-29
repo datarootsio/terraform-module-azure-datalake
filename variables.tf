1a3646521d12ad959b931621335a0fbf6aa86d86
Sam Debruyn <sam@debruyn.dev>
2020-04-29 17:50:07 +0200

    cleanup variables and add var to control sample deployment

--- variables.tf
+++ variables.tf
@@ -1,3 +1,5 @@
+# Naming & components
+
 variable "data_lake_name" {
   description = "Name of the data lake (has to be globally unique)"
   type        = string
@@ -8,37 +10,10 @@ variable "region" {
   type        = string
 }
 
-variable "service_principal_end_date" {
-  description = "End date of when the service principal is valid, formatted as a RFC3339 date string (e.g. 2018-01-01T01:02:03Z). Changing this field forces a new resource to be created."
-  type        = string
-}
-
-variable "storage_replication" {
-  description = "Type of replication for the storage accounts. See https://www.terraform.io/docs/providers/azurerm/r/storage_account.html#account_replication_type"
-  type        = string
-}
-
-variable "data_warehouse_dtu" {
-  description = "Service objective (DTU) for the created data warehouse (e.g. DW100c)"
-  type        = string
-  default     = "DW100c"
-}
-
-variable "cosmosdb_consistency_level" {
-  description = "Default consistency level for the CosmosDB account"
-  type        = string
-  default     = "Session"
-}
-
-variable "cosmosdb_db_throughput" {
-  description = "Throughput for the database inside CosmosDB"
-  type        = number
-  default     = 400
-}
-variable "databricks_sku" {
-  description = "SKU of the Databricks workspace (e.g. 'standard' or 'premium')"
-  type        = string
-  default     = "standard"
+variable "provision_sample_data" {
+  description = "Boolean to indicate if a sample data pipeline should be deployed"
+  type        = bool
+  default     = true
 }
 
 variable "data_lake_fs_raw" {
@@ -65,6 +40,50 @@ variable "data_lake_filesystems" {
   default     = []
 }
 
+variable "databricks_cluster_version" {
+  type        = string
+  description = "Runtime version of the Databricks cluster"
+}
+
+# Pricing, performance and replication
+
+variable "storage_replication" {
+  description = "Type of replication for the storage accounts. See https://www.terraform.io/docs/providers/azurerm/r/storage_account.html#account_replication_type"
+  type        = string
+}
+
+variable "data_warehouse_dtu" {
+  description = "Service objective (DTU) for the created data warehouse (e.g. DW100c)"
+  type        = string
+}
+
+variable "cosmosdb_consistency_level" {
+  description = "Default consistency level for the CosmosDB account"
+  type        = string
+}
+
+variable "cosmosdb_db_throughput" {
+  description = "Throughput for the database inside CosmosDB"
+  type        = number
+}
+
+variable "databricks_sku" {
+  description = "SKU of the Databricks workspace (e.g. 'standard' or 'premium')"
+  type        = string
+}
+
+variable "databricks_cluster_node_type" {
+  type        = string
+  description = "Node type of the Databricks cluster machines"
+}
+
+# Security
+
+variable "service_principal_end_date" {
+  description = "End date of when the service principal is valid, formatted as a RFC3339 date string (e.g. 2018-01-01T01:02:03Z). Changing this field forces a new resource to be created."
+  type        = string
+}
+
 variable "sql_server_admin_username" {
   type        = string
   description = "Username of the administrator of the SQL server"
@@ -73,27 +92,4 @@ variable "sql_server_admin_username" {
 variable "sql_server_admin_password" {
   type        = string
   description = "Password of the administrator of the SQL server"
-}
-
-variable "databricks_cluster_version" {
-  type        = string
-  description = "Runtime version of the Databricks cluster"
-}
-
-variable "databricks_cluster_node_type" {
-  type        = string
-  description = "Node type of the Databricks cluster machines"
-}
-
-locals {
-  # Common tags to be assigned to all resources
-  common_tags = {
-    DataLake = var.data_lake_name
-  }
-
-  data_lake_fs_merged           = distinct(concat([var.data_lake_fs_raw, var.data_lake_fs_cleansed, var.data_lake_fs_transformed], var.data_lake_filesystems))
-  data_lake_fs_names            = [for s in local.data_lake_fs_merged : "fs${s}${var.data_lake_name}"]
-  data_lake_fs_raw_name         = "fs${var.data_lake_fs_raw}${var.data_lake_name}"
-  data_lake_fs_clean_name       = "fs${var.data_lake_fs_cleansed}${var.data_lake_name}"
-  data_lake_fs_transformed_name = "fs${var.data_lake_fs_transformed}${var.data_lake_name}"
 }
