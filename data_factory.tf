e03323d6e2b6fb5729ce0d592ff7e974cba7415b
Sam Debruyn <sam@debruyn.dev>
2020-05-07 15:43:38 +0200

    link databricks with data factory

--- data_factory.tf
+++ data_factory.tf
@@ -14,3 +14,59 @@ resource "azurerm_data_factory_linked_service_data_lake_storage_gen2" "lsadls" {
   service_principal_id  = azuread_application.aadapp.application_id
   service_principal_key = azuread_service_principal_password.sppw.value
 }
+
+resource "azurerm_template_deployment" "lsdbks" {
+  name                = "lsdbks${var.data_lake_name}"
+  resource_group_name = azurerm_resource_group.rg.name
+
+  template_body = <<DEPLOY
+{
+    "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+    "contentVersion": "1.0.0.0",
+    "parameters": {
+        "factoryName": {
+            "type": "string"
+        },
+        "accessToken": {
+            "type": "secureString"
+        },
+        "domain": {
+            "type": "string"
+        },
+        "databricksName": {
+            "type": "string"
+        }
+    },
+    "resources": [
+        {
+            "name": "[concat(parameters('factoryName'), '/', parameters('databricksName'))]",
+            "type": "Microsoft.DataFactory/factories/linkedServices",
+            "apiVersion": "2018-06-01",
+            "properties": {
+                "annotations": [],
+                "type": "AzureDatabricks",
+                "typeProperties": {
+                    "domain": "[parameters('domain')]",
+                    "accessToken": {
+                        "type": "SecureString",
+                        "value": "[parameters('accessToken')]"
+                    },
+                    "existingClusterId": "0506-134952-froze143"
+                }
+            }
+        }
+    ]
+}
+DEPLOY
+
+
+  # these key-value pairs are passed into the ARM Template's `parameters` block
+  parameters = {
+    "factoryName"    = azurerm_data_factory.df.name
+    "accessToken"    = databricks_token.token.token_value
+    "domain"         = format("https://%s.azuredatabricks.net", azurerm_databricks_workspace.dbks.location)
+    "databricksName" = azurerm_databricks_workspace.dbks.name
+  }
+
+  deployment_mode = "Incremental"
+}
