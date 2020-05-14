a80a19d5af06cb3960c05bc17b4a995a3bbd3d94
Sam Debruyn <sam@debruyn.dev>
2020-05-14 15:30:17 +0200

    #57 destroy ARM resources + fix more deps
    This fixes #57 by destroying all the resources that were created by ARM scripts. During testing, some other issues were found related to missing dependencies (because of race conditions, the issues didn't occur every time).

---
+++ files/destroy_resource.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+token_response=$(az account get-access-token --resource https://management.core.windows.net/)
+azToken=$(jq .accessToken -r <<< "$token_response")
+curl -sSf -X DELETE "https://management.azure.com$RESOURCE_ID?api-version=2018-06-01" -H "Authorization: Bearer $azToken"
