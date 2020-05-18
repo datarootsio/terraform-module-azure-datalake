8c309a7050d34d32a963ec1c2e46eadc8d7f86d0
Sam Debruyn <sam@debruyn.dev>
2020-05-18 16:03:50 +0200

    fixed syntax errors

--- test/terraform_apply_destroy_test.go
+++ test/terraform_apply_destroy_test.go
@@ -13,23 +13,22 @@ import (
 
 const KeyDataLakeName = "data_lake_name"
 
-func getDefaultTerraformOptions() (string, *terraform.Options, error) {
+func getDefaultTerraformOptions(t *testing.T) (string, *terraform.Options, error) {
 	rand.Seed(time.Now().UnixNano())
 	sqlServerAdmin := randSeq(10)
 	sqlServerPass := randSeq(20) + strconv.Itoa(rand.Intn(1000))
 	dataLakeName := "tfadltest" + strconv.Itoa(rand.Intn(1000))
 
-	region, err := azure.GetRandomStableRegionE()
-
+	region, err := azure.GetRandomRegionE(t, nil, nil, "")
 	if err != nil {
-		return err
+		return "", nil, err
 	}
 
 	terraformOptions := &terraform.Options{
 		TerraformDir: "../",
 		Vars:         map[string]interface{}{},
 		RetryableTerraformErrors: map[string]string{
-			"*Response from server (429)",
+			"*Response from server (429)": "Failed to create notebooks due to rate limiting",
 		},
 		MaxRetries:         5,
 		TimeBetweenRetries: 30 * time.Second,
@@ -40,17 +39,17 @@ func getDefaultTerraformOptions() (string, *terraform.Options, error) {
 	terraformOptions.Vars["sql_server_admin_password"] = sqlServerPass
 	terraformOptions.Vars["region"] = region
 
-	return dataLakeName, terraformOptions
+	return dataLakeName, terraformOptions, nil
 }
 
 func TestApplyAndDestroyWithSamples(t *testing.T) {
 	t.Parallel()
 
-	name, options, err := getDefaultTerraformOptions()
+	name, options, err := getDefaultTerraformOptions(t)
 	assert.NoError(t, err)
 
 	defer terraform.Destroy(t, options)
-	err := terraform.InitAndApplyE(t, options)
+	_, err = terraform.InitAndApplyE(t, options)
 	assert.NoError(t, err)
 
 	outDataLakeName, err := terraform.OutputE(t, options, KeyDataLakeName)
@@ -62,13 +61,13 @@ func TestApplyAndDestroyWithSamples(t *testing.T) {
 func TestApplyAndDestroyWithoutSamples(t *testing.T) {
 	t.Parallel()
 
-	name, options, err := getDefaultTerraformOptions()
+	name, options, err := getDefaultTerraformOptions(t)
 	assert.NoError(t, err)
 
 	options.Vars["provision_sample_data"] = false
 
 	defer terraform.Destroy(t, options)
-	err := terraform.InitAndApplyE(t, options)
+	_, err = terraform.InitAndApplyE(t, options)
 	assert.NoError(t, err)
 
 	outDataLakeName, err := terraform.OutputE(t, options, KeyDataLakeName)
