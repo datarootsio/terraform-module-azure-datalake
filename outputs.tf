81d22180eb14562d2e5da80b7bd8cfdadbf48019
Sam Debruyn <sam@debruyn.dev>
2020-05-26 16:05:45 +0200

    fix output values for when there is no synapse

--- outputs.tf
+++ outputs.tf
@@ -3,11 +3,11 @@ output "name" {
 }
 
 output "powerbi_sql_dw_server_hostname" {
-  value = azurerm_sql_server.synapse_srv[local.create_synapse].fully_qualified_domain_name
+  value = contains(azurerm_sql_server.synapse_srv, local.create_synapse) ? azurerm_sql_server.synapse_srv[local.create_synapse].fully_qualified_domain_name : ""
 }
 
 output "powerbi_sql_dw_server_database" {
-  value = azurerm_sql_database.synapse[local.create_synapse].name
+  value = contains(azurerm_sql_database.synapse, local.create_synapse) ? azurerm_sql_database.synapse[local.create_synapse].name : ""
 }
 
 output "powerbi_sql_dw_server_user" {
@@ -15,5 +15,5 @@ output "powerbi_sql_dw_server_user" {
 }
 
 output "powerbi_sql_dw_server_password" {
-  value = random_password.sql_powerbi_viewer[local.create_synapse].result
+  value = contains(random_password.sql_powerbi_viewer, local.create_synapse) ? random_password.sql_powerbi_viewer[local.create_synapse].result : ""
 }
