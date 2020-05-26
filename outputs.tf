0755959e24e7ccd4b379ac8d0365beb1bb8aa7aa
Sam Debruyn <sam@debruyn.dev>
2020-05-26 16:12:43 +0200

    zero-based indexes!

--- outputs.tf
+++ outputs.tf
@@ -3,11 +3,11 @@ output "name" {
 }
 
 output "powerbi_sql_dw_server_hostname" {
-  value = contains(azurerm_sql_server.synapse_srv, local.create_synapse) ? azurerm_sql_server.synapse_srv[local.create_synapse].fully_qualified_domain_name : ""
+  value = contains(azurerm_sql_server.synapse_srv, 0) ? azurerm_sql_server.synapse_srv[0].fully_qualified_domain_name : ""
 }
 
 output "powerbi_sql_dw_server_database" {
-  value = contains(azurerm_sql_database.synapse, local.create_synapse) ? azurerm_sql_database.synapse[local.create_synapse].name : ""
+  value = contains(azurerm_sql_database.synapse, 0) ? azurerm_sql_database.synapse[0].name : ""
 }
 
 output "powerbi_sql_dw_server_user" {
@@ -15,5 +15,5 @@ output "powerbi_sql_dw_server_user" {
 }
 
 output "powerbi_sql_dw_server_password" {
-  value = contains(random_password.sql_powerbi_viewer, local.create_synapse) ? random_password.sql_powerbi_viewer[local.create_synapse].result : ""
+  value = contains(random_password.sql_powerbi_viewer, 0) ? random_password.sql_powerbi_viewer[0].result : ""
 }
