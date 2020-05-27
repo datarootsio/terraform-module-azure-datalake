663f52e27d9e0b86d4a1e174f9aa867b4404b727
Sam Debruyn <sam@debruyn.dev>
2020-05-27 13:51:44 +0200

    set optional git blocks for data factory

--- locals.tf
+++ locals.tf
@@ -10,8 +10,10 @@ locals {
   data_lake_fs_clean_name       = "fs${var.data_lake_fs_cleansed}${var.data_lake_name}"
   data_lake_fs_transformed_name = "fs${var.data_lake_fs_transformed}${var.data_lake_name}"
 
-  create_sample  = var.provision_sample_data && var.provision_synapse ? 1 : 0
-  create_synapse = var.provision_synapse ? 1 : 0
+  create_sample                  = var.provision_sample_data && var.provision_synapse ? 1 : 0
+  create_synapse                 = var.provision_synapse ? 1 : 0
+  create_data_factory_git_vsts   = var.data_factory_vsts_account_name == null ? [] : ["_"]
+  create_data_factory_git_github = var.data_factory_github_account_name == null ? [] : ["_"]
 
   databricks_loader_user = "DatabricksLoader"
   powerbi_viewer_user    = "PowerBiViewer"
