b97f493eeee5009985c95e5a72bda6ff100eccaf
Sam Debruyn <sam@debruyn.dev>
2020-06-04 09:30:23 +0200

    Ignore Databricks access token errors

--- test/terraform_apply_destroy_test.go
+++ test/terraform_apply_destroy_test.go
@@ -51,6 +51,7 @@ func getDefaultTerraformOptions(t *testing.T) (string, *terraform.Options, error
 			".*does not have any associated worker environments.*:":        "Databricks API was not ready for requests",
 			".*we are currently experiencing high demand in this region.*": "Azure service at capacity",
 			".*connection reset by peer.*":                                 "Temporary connectivity issue",
+			".*Error 403 Failed to retrieve tenant ID for given token.*":   "Databricks access token not valid yet",
 		},
 		MaxRetries:         5,
 		TimeBetweenRetries: 5 * time.Minute,
